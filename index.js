21581869bad34c70a733c66059c7775903648692
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2021-05-21 21:04:26 +0700

    feat(check): new server checker method

--- index.js
+++ index.js
@@ -6,18 +6,35 @@ const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync('./db/servers.json');
 const db = low(adapter);
 
+// daftar server
 const servers = [
+    // SOUTHEAST_ASIA
     { name: "SEA", ip: "13.76.128.50", port: 14301 },
+    { name: "SEA", ip: "13.76.135.145", port: 14301 },
+    { name: "SEA", ip: "52.230.66.176", port: 14301 },
+
+    // NORTH_AMERICA
     { name: "NA", ip: "110.234.17.5", port: 14300 },
+    { name: "NA", ip: "110.234.17.5", port: 14301 },
+    { name: "NA", ip: "110.234.17.51", port: 14300 },
+    { name: "NA", ip: "110.234.17.51", port: 14301 },
+
+    // KOREA
+    { name: "KO", ip: "211.56.89.200", port: 14300 },
+    { name: "KO", ip: "211.56.89.201", port: 14300 },
 ];
+
+// max percobaan ( n + 1 )
+const maxTry = 4;
 
 async function checkServer() {
     for (let i = 0; i < servers.length; i++) {
         await new Promise((resolve, reject) => {
             const server = servers[i];
 
+            // init socket
             const socket = new Socket();
-            socket.setTimeout(15000);
+            socket.setTimeout(10000);
 
             db.read();
             let status = 0;
@@ -49,14 +66,21 @@ async function checkServer() {
                 const val = db.get('servers').find({ name: server.name }).value();
 
                 if (val.status !== status) {
-                    db.get('servers').find({ name: server.name }).assign({ status }).write();
-                    console.log(`[${server.name}] > Sending notification ...`);
+                    // tambah percobaan
+                    db.get('servers').find({ name: server.name }).update('try', n => n + 1).write()
+                    console.log(`[${server.name}] Check attemp: ${Number(val.try) + 1}`);
 
-                    axios.get(`${process.env.AISHA_API}/server_update/${server.name.toLowerCase()}`)
-                        .then((res) => true )
-                        .catch(function (error) {
-                            console.error(error);
-                        });
+                    // cek x kali percobaan, apakah benar-benar down atau tidak
+                    if (val.try >= maxTry) {
+                        db.get('servers').find({ name: server.name }).assign({ try: 0, status }).write();
+                        console.log(`[${server.name}] > Sending notification ...`);
+
+                        axios.get(`${process.env.AISHA_API}/server_update/${server.name.toLowerCase()}`)
+                            .then((res) => true )
+                            .catch(function (error) {
+                                console.error(error);
+                            });
+                    }
                 }
 
                 console.log(`[${server.name}] Closed!`);
@@ -73,6 +97,7 @@ initDb = () => {
         return {
             name: srv.name,
             status: 1,
+            try: 0,
         };
     });
 
@@ -80,4 +105,4 @@ initDb = () => {
 };
 
 initDb();
-setInterval(checkServer, 60000);
+setInterval(checkServer, 30000);
